884aba77ef7c152d76fa65cfa72a7d18e69dbdfa
toshi <toshiyukitakaichi@gmail.com>
2026-04-25 18:25:59 +0900

    fix mirror camera

--- Assets/toshi.VLiveKit/VLiveCameraUnit/Runtime/MirrorCamera/MirrorCamera.cs
+++ Assets/toshi.VLiveKit/VLiveCameraUnit/Runtime/MirrorCamera/MirrorCamera.cs
@@ -9,13 +9,13 @@ public class MirrorCamera : MonoBehaviour
 
     void Start()
     {
-        probe = GetComponent<ReflectionProbe>();
+        this.probe = GetComponent<ReflectionProbe>();
     }
 
     void Update()
     {
         //y軸は-1をかけて逆側に配置する
-        probe.transform.position = new Vector3(Camera.main.transform.position.x,
+        this.probe.transform.position = new Vector3(Camera.main.transform.position.x,
                                                     Camera.main.transform.position.y * -1,
                                                     Camera.main.transform.position.z);
 
